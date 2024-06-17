69894d6bfbcebdf981ed11db80348dd159c59e67
Kseniia Machnieva <164485727+Ksenia-m4@users.noreply.github.com>
2024-06-17 19:40:19 +0200

    callback fn

---
+++ js/callback-fn.js
@@ -0,0 +1,19 @@
+// function registerGuest(name, callback) {
+//   console.log(`Реєструємо гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Манго", function greet(name) {
+//   // если колбєк функция маленькая записіваем ее внутри скобок
+//   console.log(`Ласкаво просимо ${name}.`);
+// });
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// numbers.forEach(function (number, index) {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
+// // Анонімна стрілочна функція
+// numbers.forEach((number, index) => {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// });
